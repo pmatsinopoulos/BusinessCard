b40afa6a62647a729b70d7edac302a0e155c399e
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-08-01 12:38:38 +0300

    Added the contact details

--- app/src/main/java/com/mixlr/panos/businesscard/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/businesscard/MainActivity.kt
@@ -8,8 +8,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,7 +49,10 @@ class MainActivity : ComponentActivity() {
                 ) {
                     BusinessCard(
                         jobTitle = stringResource(R.string.job_title),
-                        name = stringResource(R.string.name)
+                        name = stringResource(R.string.name),
+                        phoneNumber = stringResource(R.string.phone_number),
+                        socialText = stringResource(R.string.social_text),
+                        email = stringResource(R.string.email_address)
                     )
                 }
             }
@@ -50,12 +64,26 @@ class MainActivity : ComponentActivity() {
 fun BusinessCard(
     jobTitle: String,
     name: String,
+    phoneNumber: String,
+    socialText: String,
+    email: String,
     modifier: Modifier = Modifier
 ) {
-    MainHeader(
-        jobTitle = jobTitle,
-        name = name
-    )
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        MainHeader(
+            jobTitle = jobTitle,
+            name = name
+        )
+        Spacer(Modifier.height(200.dp))
+        ContactDetails(
+            phoneNumber = phoneNumber,
+            socialText = socialText,
+            email = email
+        )
+    }
 }
 
 @Composable
@@ -71,6 +99,78 @@ fun MainHeader(
         Logo()
         Name(name = name)
         JobTitle(jobTitle = jobTitle)
+    }
+}
+
+@Composable
+fun ContactDetails(
+    phoneNumber: String,
+    socialText: String,
+    email: String,
+    modifier: Modifier = Modifier
+) {
+    val telephoneIcon: ImageVector = Icons.Rounded.Call
+    val socialIcon: ImageVector = Icons.Rounded.Share
+    val envelopeIcon: ImageVector = Icons.Rounded.Email
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .padding(bottom = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ContactDetail(
+            icon = telephoneIcon,
+            text = phoneNumber
+        )
+        ContactDetail(icon = socialIcon, text = socialText)
+        ContactDetail(icon = envelopeIcon, text = email)
+    }
+}
+
+@Composable
+fun ContactDetail(
+    icon: ImageVector,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier.padding(bottom = 10.dp)
+    ) {
+        ContactIcon(
+            icon = icon
+        )
+        ContactInfo(
+            text = text
+        )
+    }
+}
+
+@Composable
+fun ContactIcon(
+    icon: ImageVector,
+    modifier: Modifier = Modifier
+) {
+    Column(
+    ) {
+        Icon(
+            icon,
+            contentDescription = null,
+            tint = colorResource(id = R.color.android_green_dark)
+        )
+    }
+}
+
+@Composable
+fun ContactInfo(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.width(220.dp)
+            .padding(horizontal = 10.dp)
+    ) {
+        Text(
+            text = text
+        )
     }
 }
 
@@ -116,7 +216,10 @@ fun BusinessCardPreview() {
     BusinessCardTheme {
         BusinessCard(
             jobTitle = stringResource(R.string.job_title),
-            name = stringResource(R.string.name)
+            name = stringResource(R.string.name),
+            phoneNumber = stringResource(R.string.phone_number),
+            socialText = stringResource(R.string.social_text),
+            email = stringResource(R.string.email_address)
         )
     }
 }
